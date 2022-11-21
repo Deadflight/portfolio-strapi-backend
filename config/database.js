2f0bc369c9cbb6ef8eb4038087c3979da56fe22c
Carlos <correamillancarlos@gmail.com>
2022-11-20 21:10:26 -0400

    playground always

--- config/database.js
+++ config/database.js
@@ -1,15 +1,11 @@
-module.exports = ({ env }) => ({
+const path = require("path");
+
+module.exports = () => ({
   connection: {
-    client: "postgres",
+    client: "sqlite",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
-      ssl: env("DATABASE_SSL", false),
+      filename: path.join(__dirname, "..", ".tmp/data.db"),
     },
-    debug: false,
+    useNullAsDefault: true,
   },
 });
